252b71de3e5acfe61f0de38b3d6e8687f8802572
SellfC <110660214+SellfC@users.noreply.github.com>
2023-08-07 09:09:38 +0300

    Delete falestatsadv.lua

--- falestatsadv.lua
+++
@@ -1,3 +0,0 @@
-game.Players.LocalPlayer.PlayerGui.StatReroll.grid.UnitStats.Attack.Main.TextLabel.Text = "+30.4%" -- attack
-game.Players.LocalPlayer.PlayerGui.StatReroll.grid.UnitStats.Cooldown.Main.TextLabel.Text = "+15%" -- cooldown
-game.Players.LocalPlayer.PlayerGui.StatReroll.grid.UnitStats.Range.Main.TextLabel.Text = "+15%" -- range
